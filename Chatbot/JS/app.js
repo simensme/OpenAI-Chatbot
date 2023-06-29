8313e675a3d644c47e7701212a428056fb1dc31a
Simen <106187102+simensme@users.noreply.github.com>
2023-06-29 17:59:33 +0200

    Scroll and showChat
    Scroll to the bottom + function to show chat.

--- Chatbot/JS/app.js
+++ Chatbot/JS/app.js
@@ -1,7 +1,57 @@
-import {nameAndWelcomeMessage} from "./Services/Functions.js";
+import {nameAndWelcomeMessage} from "./Services/functions.js";
 
 // Load name and welcome message
 document.addEventListener('DOMContentLoaded', e => {
     nameAndWelcomeMessage();
+
+
+         // Chatbot messages - Handlechatbot function
+         let textboxNode = document.getElementById('textbox');
+         const showChat = (type, message) => {
+             let chatContainerNode = document.createElement('div');
+             chatContainerNode.classlIst.add('message', type);
+             let chatText = document.createElement('p');
+             chatText.innerHTML = message;
+             chatContainerNode.appendChild(chatText);
+
+             textboxNode.appendChild(chatContainerNode);
+             textboxNode.scrollTop = chatLog.scrollHeight;
+         };
+
+
+         // Check if you have internet
+         const onlineCheck = () => {
+            return navigator.onLine;
+         };
+
+         console.log(onlineCheck());
+
+
+
+         // Loader
+         const triggerLoader = () => {
+            document.querySelector('.load-container').style.display = 'flex';
+         };
+
+         const stopLoader = () => {
+            document.querySelector('.load-container').style.display = 'none';
+         };
+
+         
+         // Focus on the bottom of the chat.
+         let scrollPosition = 0;
+         const unscroll = () => {
+            textboxNode.scrollTop = scrollPosition;
+         }
+         const scrollText = () => {
+            let scrolledToBottom = textboxNode.scrollHeight - textboxNode.clientHeight <= textboxNode.scrollTop + 1;
+            textboxNode.scrollTop = textboxNode.scrollHeight;
+            if (scrolledToBottom) {
+                unscroll();
+            }
+         }
+         scrollText();
+    
+
 });
 
